f017e495d6d6d2455614ffd379a2f3db1f025803
ahmed.ma7goub <ahmed.ma7goub@gamil.com>
2025-07-01 16:56:39 +0300

    fix: Apply NDK version and enable desugaring2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,11 @@ plugins {
 android {
     namespace = "com.example.rafiq_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // Enables core library desugaring, which is required by some plugins.
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -41,4 +43,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
